3e4d41dddfebf1e326636ee01ac5eed07048f46a
reductant01 <reductant01@pusan.ac.kr>
2024-07-01 15:13:27 +0900

    c plus plus

--- chap2/2-1.arr1.cpp
+++ chap2/2-1.arr1.cpp
@@ -1,7 +1,7 @@
 //2-1. arr1.cpp 실습예제 2.1
 #include <stdio.h>
 #include <stdlib.h>
-//1 차원 배열에 대한 pointer 사용
+//1 차원 배열에 대한 pointer 사용!
 void main(){
     int odd[5] = {3, 5, 7, 9, 11};
     printf("&odd[0] = %p\n", &odd[0]);
